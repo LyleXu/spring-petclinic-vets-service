5c1ce441ab8db9932a5ab9225e57c85b27f3fe74
luxu-ms <92287281+luxu-ms@users.noreply.github.com>
2022-04-07 17:34:16 +0800

    add bean for http trace

--- src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
+++ src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
@@ -16,9 +16,12 @@
 package org.springframework.samples.petclinic.vets;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.samples.petclinic.vets.system.VetsProperties;
 
 /**
@@ -32,4 +35,9 @@ public class VetsServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VetsServiceApplication.class, args);
 	}
+
+	@Bean
+    public HttpTraceRepository htttpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    };
 }
